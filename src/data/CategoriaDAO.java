d4ba6c1a7c96c864f21f2ca4760bc363a918ecb2
Richard Andres Useche Lopez <ruseche@ucompensar.edu.co>
2026-04-16 09:48:30 -0500

    feat: CategoriaDAO archive finished

--- src/data/CategoriaDAO.java
+++ src/data/CategoriaDAO.java
@@ -35,34 +35,141 @@ public class CategoriaDAO implements CrudSimpleInterface<Categoria> {
                 System.out.println("COMMIT");
 
             }
+            
+            ps.close();
+            rs.close();
+            
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, e.getMessage());
+        } finally {
+            ps = null;
+            rs = null;
+            CON.desconectar();
         }
         return registros;
     }
 
     @Override
     public boolean insertar(Categoria obj) {
-        return false;
+        resp = false;
+        try {
+            ps = CON.conectar().prepareStatement("INSERT INTO categoria (nombre, descripcion ,activo ) VALUES (?,?,1)");
+            ps.setString(2,obj.getNombre());
+            ps.setString(1, obj.getDescripcion());
+            if (ps.executeUpdate() > 0) {
+                resp = true;
+            }
+            ps.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        } finally {
+            ps = null;
+            CON.desconectar();
+        }
+        return resp;
     }
 
     @Override
     public boolean actualizar(Categoria obj) {
-        return false;
+        resp = false;
+        try {
+            ps = CON.conectar().prepareStatement("UPDATE categoria SET nombre =?, descripcion=?, id =?");
+            ps.setString(2,obj.getNombre());
+            ps.setString(1, obj.getDescripcion());
+            ps.setInt(3, obj.getId());
+            if (ps.executeUpdate() > 0) {
+                resp = true;
+            }
+            ps.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        } finally {
+            ps = null;
+            CON.desconectar();
+        }
+        return resp;
     }
 
     @Override
     public boolean desactivar(int id) {
-        return false;
+        resp = false;
+        try {
+            ps = CON.conectar().prepareStatement("UPDATE categoria SET activo=0, WHERE id =?");
+            ps.setInt(1 ,id);
+            if (ps.executeUpdate() > 0) {
+                resp = true;
+            }
+            ps.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        } finally {
+            ps = null;
+            CON.desconectar();
+        }
+        return resp;
     }
-
+    
+    @Override
+    public boolean activar(int id) {
+        resp = false;
+        try {
+            ps = CON.conectar().prepareStatement("UPDATE categoria SET activo=1, WHERE id =?");
+            ps.setInt(1 ,id);
+            if (ps.executeUpdate() > 0) {
+                resp = true;
+            }
+            ps.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        } finally {
+            ps = null;
+            CON.desconectar();
+        }
+        return resp;
+    }
+    
     @Override
     public int total() {
-        return 0;
+        int totalRegistros=0;
+        try {
+            ps = CON.conectar().prepareStatement("SELECT COUNT(id) categoria ");
+            rs= ps.executeQuery();
+            while (rs.next()) {                
+                totalRegistros = rs.getInt("COUNT(id)");
+            }
+            ps.close();
+            rs.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        } finally {
+            ps = null;
+            rs = null;
+            CON.desconectar();
+        }
+        return totalRegistros;
     }
 
     @Override
     public boolean existe(String texto) {
-        return false;
+        resp = false;
+        try {
+            ps = CON.conectar().prepareStatement("SELECT nombre FROM categoria WHERE nombre=?");
+            ps.setString(1,texto);
+            rs = ps.executeQuery();
+            rs.last();
+            
+            if (rs.getRow() > 0) {
+                resp = true;
+            }
+            ps.close();
+            rs.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        } finally {
+            ps = null;
+            rs = null;
+            CON.desconectar();
+        }
+        return resp;
     }
 }
--- src/data/interfaces/CrudSimpleInterface.java
+++ src/data/interfaces/CrudSimpleInterface.java
@@ -12,6 +12,7 @@ public interface CrudSimpleInterface<T> {
    public boolean insertar (T obj);
    public boolean actualizar(T obj);
    public boolean desactivar (int id);
+   public boolean activar (int id);
    public int total();
    public boolean existe(String texto);
 }
